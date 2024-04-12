ae6733271e1d4fcab3b3c91fa3eacef17da2d429
prince3453 <princeghoghari9@gmail.com>
2024-04-12 11:33:19 -0400

    navbar

---
+++ src/component/Hero/hero.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+export const hero = () =>{
+    return(
+        <section>
+        <div>
+            <h1>Hi, I&#39;m Prince</h1>
+            <p>I am Data Engineer and software developer for the 2.5 years of Experience in the Python, Django, ETL pipeline, and Azure Databricks. Reach out if you would like to learn more!</p>
+        </div>
+        </section>
+    )
+}
