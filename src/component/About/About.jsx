953cb0d342d66e636eeaa54da82ce39c8b902c79
prince3453 <princeghoghari9@gmail.com>
2024-04-17 14:35:48 -0400

    About page content and color changed

--- src/component/About/About.jsx
+++ src/component/About/About.jsx
@@ -12,8 +12,8 @@ export const About =  () => {
                 <li className={styles.aboutItem}>
                     <img src={getImageUrl("about/cursorIcon.png")} alt="cursor Icon" />
                     <div className={styles.aboutItemtext}>
-                        <h3>Data engineer</h3>
-                        <p>Etl pipeline experience building in the business solutions</p>
+                        <h3>Data Engineer</h3>
+                        <p>Expert in building robust ETL pipelines and managing databases using advanced tools like Snowflake and Azure Data Factory, ensuring data integrity and optimal performance for data-driven decision making.</p>
                     </div>
                 </li>
 
@@ -21,15 +21,15 @@ export const About =  () => {
                     <img src={getImageUrl("about/serverIcon.png")} alt="Server Icon" />
                     <div className={styles.aboutItemtext}>
                         <h3>Data Scientist</h3>
-                        <p>Etl pipeline experience building in the business solutions</p>
+                        <p>Proficient in applying statistical modeling and machine learning algorithms to extract actionable insights from large datasets, enhancing business strategies and operational efficiency.</p>
                     </div>
                 </li>
 
                 <li className={styles.aboutItem}>
                     <img src={getImageUrl("about/uiIcon.png")} alt="ui Icon" />
                     <div className={styles.aboutItemtext}>
-                        <h3>Python Developer</h3>
-                        <p>Develope the scalable and maintainable softwares</p>
+                        <h3>Software Developer</h3>
+                        <p>Experienced in developing responsive, scalable web applications using technologies like Django and ReactJS, focusing on creating intuitive user interfaces and maintaining high software quality through effective backend management.</p>
                     </div>
                 </li>
             </ul>
