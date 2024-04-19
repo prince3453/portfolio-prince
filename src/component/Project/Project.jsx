4ae82c0fe7ca9c13c6adf8bbaee3f815c6926df5
prince3453 <princeghoghari9@gmail.com>
2024-04-19 17:32:29 -0400

    Added the project section and contact section

--- src/component/Project/Project.jsx
+++ src/component/Project/Project.jsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import projects from '../../data/project.json';
 import { ProjectCard } from './ProjectCard';
+import styles from './Project.module.css';
 
 export const Project =() => {
     return (
-        <section>
-            <h2>Project</h2>
-            <div>
+        <section className={styles.container}>
+            <h2 className={styles.title}>Project</h2>
+            <div className={styles.project}>
                 {
                     projects.map((project, id) => {
                         return <ProjectCard key={id} project={project} />;
